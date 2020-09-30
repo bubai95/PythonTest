d976e2c5999d3d908e75b1a87701bf8eee6162a0
bubai95 <72161885+bubai95@users.noreply.github.com>
2020-10-01 00:22:45 +0530

    Update DelChar.py
    Added more comment

--- Swayam/DelChar.py
+++ Swayam/DelChar.py
@@ -2,6 +2,8 @@
 Write a function delchar(s,c) that takes as input strings s and c, where c has length 1 
 (i.e., a single character), and returns the string obtained by deleting all 
 occurrences of c in s. If c has length other than 1, the function should return s
+
+SWAYAM NPTEL 2nd week Data Structure using Python Programming Problems
 """
 def delchar(s,c):
     if len(c)==1:
